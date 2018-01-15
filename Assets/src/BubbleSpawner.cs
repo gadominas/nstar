6ae68abf541be4b29f8164bc70dec2873d4caa23
Gediminas Siutilas <gsiutilas@eisgroup.com>
2018-01-15 12:28:34 +0200

    initial seed

---
+++ Assets/src/BubbleSpawner.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleSpawner : MonoBehaviour {
+  public GameObject bubble;
+
+  // DEBUG
+  public GameObject lx;
+  public GameObject ly;
+  public GameObject rx;
+  public GameObject ry;
+
+  float vlx = .0f;
+  float vly = .0f;
+  float vrx = .0f;
+  float vry = .0f;
+  // DEBUG
+
+  private NStar parentStar;
+  private NStar currentStar;
+
+  ArrayList starMap = new ArrayList();
+
+  void Update() {
+    if (Input.GetKey(KeyCode.R)) {
+      parentStar = null;
+      starMap.Clear();
+      vlx = vly = vrx = vry = .0f;
+    }
+
+    if (Input.GetMouseButtonDown(0) && Input.GetKey(KeyCode.A)) {
+      spawnBuble();
+    }
+
+    updateUniverseBoundMarker();
+  }
+
+  private updateqStarMap(){
+    
+      Vector2 touchPoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+      Vector3 worldPoint = Camera.main.ScreenToWorldPoint(touchPoint);
+
+      bool overlaped = starCollider.OverlapPoint(worldPoint);
+      highlightIfOverlaps(overlaped);
+      selectIfOverlaps(overlaped);
+ 
+  }
+
+  private void spawnBuble() {
+    Vector2 touchPoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+    Vector3 worldPoint = Camera.main.ScreenToWorldPoint(touchPoint);
+    worldPoint.z = .0f;
+
+    currentStar = Instantiate(bubble, transform).GetComponent<NStar>();
+    currentStar.transform.position = worldPoint;
+
+    starMap.Add(currentStar.GetComponent<NStar>());
+    currentStar.GetComponent<NStar>().join(getSelected());
+
+    if (parentStar != null) {
+      parentStar.setSelected();
+    }
+
+    parentStar = currentStar;
+    updateUniverseBoundMarker();
+  }
+
+  private NStar getSelected(){
+    foreach (NStar star in starMap) {
+      if (star.isSelected()) {
+        return star;
+      }
+    }
+     
+    return parentStar;
+  }
+
+  private void updateUniverseBoundMarker() {
+    updateUniverseBound();
+
+    lx.transform.position = new Vector3(vlx-5, vly+5);
+    ly.transform.position = new Vector3(vlx-5, vry-5);
+    rx.transform.position = new Vector3(vrx+5, vly+5);
+    ry.transform.position = new Vector3(vrx+5, vry-5);
+  }
+
+  private void updateUniverseBound() {
+    foreach (NStar star in starMap) {
+      Vector3 position = star.transform.position;
+
+      vlx = Mathf.Min(position.x, vlx);
+      vly = Mathf.Max(position.y, vly);
+      vrx = Mathf.Max(position.x, vrx);
+      vry = Mathf.Min(position.y, vry);
+    }
+  }
+}
---
+++ Assets/src/NStar.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NStar : MonoBehaviour {
+  public LineRenderer lineRenderer;
+  public CircleCollider2D starCollider;
+
+  public GameObject selectedSprite;
+  public GameObject starSprite;
+
+  private ArrayList parentStars = new ArrayList();
+  private ArrayList childStars = new ArrayList();
+
+  private float starPositionInBubble;
+  private Vector3 starPosition = new Vector3();
+  private bool selected = false;
+  private bool highlight = false;
+
+  private Color normalStartColor;
+  private Color normalEndColor;
+  private Color highlightStartColor;
+  private Color highlightEndColor;
+
+  public void join(NStar parent) {
+    starPositionInBubble = Random.Range(0, 360);
+
+    if (parent != null) {
+      parent.setUnSelected();
+      parent.setChild(this);
+
+      parentStars.Add(parent);
+    }
+  }
+
+  public void setChild(NStar child) {
+    childStars.Add(child);
+  }
+
+  public void highlightChildBranch() {
+    highlight = true;
+
+    if (childStars.Count > 0) {
+      foreach (NStar star in childStars) {
+        star.highlightChildBranch();
+      }
+    }
+  }
+
+  public void clearHighlightFromParentBranch() {
+    highlight = false;
+
+    if (parentStars.Count > 0) {
+      foreach (NStar star in parentStars) {
+        star.clearHighlightFromParentBranch();
+      }
+    }
+  }
+
+  public Vector3 getStarPosition() {
+    return getStarPosition(starPositionInBubble);
+  }
+
+  public float getBubbleRadius() {
+    return starCollider.radius;
+  }
+
+  public void setSelected() {
+    selected = true;
+  }
+
+  public void setUnSelected() {
+    selected = false;
+  }
+
+  public bool isSelected() {
+    return selected;
+  }
+
+  void Start() {
+    normalStartColor = lineRenderer.startColor;
+    normalEndColor = lineRenderer.endColor;
+
+    highlightStartColor = Color.blue;
+    highlightEndColor = Color.green;
+  }
+
+  private Vector3 getStarPosition(float angle) {
+    float radius = starCollider.radius * 0.6f;
+    starPosition.x = transform.localPosition.x + radius * Mathf.Cos(angle);
+    starPosition.y = transform.localPosition.y + radius * Mathf.Sin(angle);
+
+    return starPosition;
+  }
+
+  void Update() {
+    if (Input.GetMouseButtonDown(0)) {
+      Vector2 touchPoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+      Vector3 worldPoint = Camera.main.ScreenToWorldPoint(touchPoint);
+
+      bool overlaped = starCollider.OverlapPoint(worldPoint);
+      highlightIfOverlaps(overlaped);
+      selectIfOverlaps(overlaped);
+    }
+
+    selectedSprite.SetActive(selected);
+  }
+
+  private void highlightIfOverlaps(bool overlaped) {
+    if (overlaped) {
+      clearHighlightFromParentBranch();
+      highlightChildBranch();
+    }
+
+    highlightConnections();
+  }
+
+  private void selectIfOverlaps(bool overlaped) {
+    selected = overlaped;
+  }
+
+  void FixedUpdate() {
+    updateConnections();
+    starSprite.transform.position = getStarPosition();
+  }
+
+  private void updateConnections() {
+    if (parentStars.Count > 0) {
+      int lineCount = 0;
+
+      foreach (NStar star in parentStars) {
+        lineCount += 2;
+        lineRenderer.positionCount = lineCount;
+        lineRenderer.SetPosition(lineCount -2, getStarPosition());
+        lineRenderer.SetPosition(lineCount -1, star.getStarPosition());
+      }
+    }
+  }
+
+  private void highlightConnections(){
+    if (highlight) {
+      lineRenderer.startColor = highlightStartColor;
+      lineRenderer.endColor = highlightEndColor;
+    } else {
+      lineRenderer.startColor = normalStartColor;
+      lineRenderer.endColor = normalEndColor;
+    }
+  }
+}
